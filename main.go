01b8c5f24871111d8212a3ea8771e298770dd5b3
gcdd1993 <gcwm99@gmail.com>
2024-05-23 15:11:51 +0800

    支持设置命令行参数入参

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"db-doc/database"
 	"db-doc/model"
+	"flag"
 	"fmt"
 	"os"
 )
@@ -13,49 +14,52 @@ var dbConfig model.DbConfig
 
 func main() {
 	fmt.Printf("Welcome to the database document generation tool, the current version is %s \n", version)
-	fmt.Println("? Database type:\n1:MySQL or MariaDB\n2:SQL Server\n3:PostgreSQL")
-	// db type
-	fmt.Scanln(&dbConfig.DbType)
+
+	// 定义命令行参数
+	dbTypePtr := flag.Int("db-type", 1, "Database type: 1:MySQL or MariaDB, 2:SQL Server, 3:PostgreSQL")
+	hostPtr := flag.String("db-host", "127.0.0.1", "Database host")
+	portPtr := flag.Int("db-port", 3306, "Database port")
+	userPtr := flag.String("db-user", "root", "Database username")
+	passwordPtr := flag.String("db-password", "123456", "Database password")
+	databasePtr := flag.String("db-name", "", "Database name")
+	docTypePtr := flag.Int("doc-type", 1, "Document type: 1:Online, 2:Offline")
+
+	// 解析命令行参数
+	flag.Parse()
+
+	// 赋值给dbConfig
+	dbConfig.DbType = *dbTypePtr
+	dbConfig.Host = *hostPtr
+	dbConfig.Port = *portPtr
+	dbConfig.User = *userPtr
+	dbConfig.Password = *passwordPtr
+	dbConfig.Database = *databasePtr
+	dbConfig.DocType = *docTypePtr
+
+	// 检查DbType是否在有效范围内
 	if dbConfig.DbType < 1 || dbConfig.DbType > 3 {
-		fmt.Println("wrong number, will exit ...")
-		os.Exit(0)
+		fmt.Println("wrong number for --db-type, will exit ...")
+		os.Exit(1)
 	}
+
+	// 根据DbType设置默认端口和用户名
 	GetDefaultConfig()
-	// db host
-	fmt.Println("? Database host (127.0.0.1) :")
-	fmt.Scanln(&dbConfig.Host)
-	// db port
-	fmt.Printf("? Database port (%d) :\n", dbConfig.Port)
-	fmt.Scanln(&dbConfig.Port)
-	// db user
-	fmt.Printf("? Database username (%s) :\n", dbConfig.User)
-	fmt.Scanln(&dbConfig.User)
-	// db password
-	fmt.Println("? Database password (123456) :")
-	fmt.Scanln(&dbConfig.Password)
-	// db name
-	fmt.Println("? Database name:")
-	fmt.Scanln(&dbConfig.Database)
-	// doc type
-	fmt.Println("? Document type:\n1:Online\n2:Offline")
-	fmt.Scanln(&dbConfig.DocType)
+
 	// generate
 	database.Generate(&dbConfig)
 }
 
 // GetDefaultConfig get default config
 func GetDefaultConfig() {
-	dbConfig.Host = "127.0.0.1"
-	dbConfig.Password = "123456"
-	if dbConfig.DbType == 1 {
+	// 根据DbType设置默认端口和用户名
+	switch dbConfig.DbType {
+	case 1:
 		dbConfig.Port = 3306
 		dbConfig.User = "root"
-	}
-	if dbConfig.DbType == 2 {
+	case 2:
 		dbConfig.Port = 1433
 		dbConfig.User = "sa"
-	}
-	if dbConfig.DbType == 3 {
+	case 3:
 		dbConfig.Port = 5432
 		dbConfig.User = "postgres"
 	}
